878a3462ae008e78be9677f9d8ceca7fdb28aa6e
steve-jimenez-ucf <144864981+steve-jimenez-ucf@users.noreply.github.com>
2023-09-27 13:27:21 -0400

    Added Stock app and related migrations

--- stocks/models.py
+++ stocks/models.py
@@ -1,38 +1,39 @@
+from datetime import date
+
 from django.db import models
-
-
-class Portfolio(models.Model):
-    """A group of stocks held in a portfolio"""
-    name = models.CharField(max_length=20, help_text="Abbreviated name of the portfolio.")
-    description = models.CharField(max_length=100, help_text="Description of the portfolio.")
-
-    def __str__(self):
-        return self.name + " - " + self.description
 
 
 class Stock(models.Model):
     """A publicly traded stock"""
     ticker = models.CharField(max_length=10, help_text="The ticker associated with the stock.")
     company = models.CharField(max_length=10, help_text="The name of the company that issued the stock.")
-    portfolio = models.ForeignKey(Portfolio,
-                                  on_delete=models.CASCADE)  # For purposes of our project, a Stock can only belong to one Portfolio
 
     def __str__(self):
         return self.ticker + " - " + self.company
 
 
+class Portfolio(models.Model):
+    """A group of stocks held in a portfolio"""
+    name = models.CharField(max_length=20, help_text="Abbreviated name of the portfolio.")
+    description = models.CharField(max_length=100, help_text="Description of the portfolio.")
+    stocks = models.ManyToManyField('Stock')
+
+    def __str__(self):
+        return self.name + " - " + self.description
+
+
 class StockPrice(models.Model):
     """The closing stock price on a given date"""
 
-    ticker = models.ForeignKey(Stock, on_delete=models.CASCADE)
-    date = models.DateField(help_text="Date for the stock price.")
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    date: date = models.DateField(help_text="Date for the stock price.")
     price = models.DecimalField(help_text="The closing stock price.", max_digits=18, decimal_places=4)
 
     def __str__(self):
-        return self.ticker.ticker + " - " + self.date + " - " + self.price
+        return self.stock.ticker + " - " + str(self.date) + " - " + str(self.price)
 
 
-class Sentiment(models.Model):
+class SentimentScore(models.Model):
     """Represents the sentiment score for a given stock on a given date"""
 
     class SentimentRecommendation(models.TextChoices):
@@ -40,17 +41,17 @@ class Sentiment(models.Model):
         HOLD = "HOLD", "Hold"
         SELL = "SELL", "Sell"
 
-    ticker = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     date = models.DateField(help_text="Date for which the sentiment score was calculated.")
     score = models.DecimalField(help_text="The sentiment score.", max_digits=18, decimal_places=16)
     recommendation = models.CharField(help_text="The action recommended for the stock.",
                                       choices=SentimentRecommendation.choices, max_length=20)
 
     def __str__(self):
-        return self.ticker.ticker + " - " + self.date + " - " + self.score
+        return self.stock.ticker + " - " + str(self.date) + " - " + str(self.score)
 
 
-class SentimentDetail(models.Model):
+class Sentiment(models.Model):
     """Represents the individual sentiments captured for a given stock on a given date"""
 
     class SentimentChoices(models.IntegerChoices):
@@ -58,9 +59,9 @@ class SentimentDetail(models.Model):
         NEUTRAL = 0
         POSITIVE = 1
 
-    ticker = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     date = models.DateField(help_text="Date for which the sentiment score was calculated.")
     sentiment = models.IntegerField(choices=SentimentChoices.choices)
 
     def __str__(self):
-        return self.ticker.ticker + " - " + self.date + " - " + self.sentiment
+        return self.stock.ticker + " - " + str(self.date) + " - " + str(self.sentiment)
