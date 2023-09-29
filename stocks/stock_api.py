4848709b4b911b59b6ccca61890ffe4c4d240c1e
steve-jimenez-ucf <144864981+steve-jimenez-ucf@users.noreply.github.com>
2023-09-28 22:33:17 -0400

    Improvements to stock price lookup

--- stocks/stock_api.py
+++ stocks/stock_api.py
@@ -1,9 +1,12 @@
-from finnhub import Client
+import os
 from datetime import datetime, timezone
 
+from finnhub import Client
+
+
 # https://finnhub.io/docs/api/stock-candles
-def get_daily_closing_stock_prices(finnhub_api_key, ticker, dt_from, dt_to):
-    price_dict = {}
+def get_finhub_stock_candles(ticker, dt_from, dt_to):
+    finnhub_api_key = os.getenv("FINNHUB_API_KEY")
 
     # Convert from datetime to UNIX timestamp
     int_from = int(round(dt_from.timestamp()))
@@ -14,6 +17,12 @@ def get_daily_closing_stock_prices(finnhub_api_key, ticker, dt_from, dt_to):
 
     candles = finnhub_client.stock_candles(ticker, 'D', int_from, int_to)
     # print(candles)
+    return candles
+
+
+def get_closing_stock_prices_date_range(ticker, dt_from, dt_to):
+    price_dict = {}
+    candles = get_finhub_stock_candles(ticker, dt_from, dt_to)
 
     for dt, price in zip(candles['t'], candles['c']):
         price_dict[datetime.fromtimestamp(dt, timezone.utc).strftime("%Y-%m-%d")] = price
@@ -21,11 +30,14 @@ def get_daily_closing_stock_prices(finnhub_api_key, ticker, dt_from, dt_to):
     return price_dict
 
 
+def get_closing_stock_price_on_date(ticker, dt_price):
+    stock_price = 0
+    candles = get_finhub_stock_candles(ticker, dt_price, dt_price)
 
+    if candles and candles['s'] != 'no_data':
+        stock_price = candles['c'][0]
 
-
-
-
+    return stock_price
 
 # from dotenv import load_dotenv
 # load_dotenv()  # take environment variables from .env.
--- stocks/views.py
+++ stocks/views.py
@@ -1,32 +1,47 @@
 from datetime import datetime, timezone
-import os
 
 from django.shortcuts import render
 
 from .models import Stock, StockPrice
-from .stock_api import get_daily_closing_stock_prices
+from .stock_api import get_closing_stock_price_on_date, get_closing_stock_prices_date_range
 
 
 def stocks(request):
-    stock_price_count = load_db_stock_prices()
-    return render(request, "stocks/stocks.html", {"stock_price_count": stock_price_count})
+    # http://127.0.0.1:8000/stocks?ticker=AAPL&price_date=2023-07-31
+    ticker = request.GET.get("ticker") or "AAPL"
+    price_date = request.GET.get("price_date") or datetime.today().strftime('%Y-%m-%d')
+    price_date = datetime.strptime(price_date, "%Y-%m-%d")
+    # price_date = datetime(price_date.year, price_date.month, price_date.day)
+    print('View for stocks "{}" "{}"'.format(ticker, price_date))
+    price = get_or_lookup_stock_price(ticker, price_date)
+    return render(request, "stocks/stocks.html", {"ticker": ticker, "price_date": price_date, "price": price})
 
 
-# https://finnhub.io/docs/api/stock-candles
-def load_db_stock_prices():
+def get_or_lookup_stock_price(ticker, price_date):
+    stock_price = 0
+    s = Stock.objects.get(ticker=ticker)
+    sp = StockPrice.objects.filter(stock=s, date=price_date).first()
+    if sp:
+        stock_price = sp.price
+        print('Found StockPrice "{}" "{}" "{}"'.format(s.ticker, sp.date, sp.price))
+    else:
+        stock_price = get_closing_stock_price_on_date(ticker, price_date)
+        sp = StockPrice.objects.create(stock=s, date=price_date, price=stock_price)
+        print('Created StockPrice "{}" "{}" "{}"'.format(s.ticker, sp.date, sp.price))
+
+    return stock_price
+
+
+def test_bulk_load_db_stock_price_history():
     dt_from = datetime(2022, 6, 1, 0, 0, 0, 0, timezone.utc)
     dt_to = datetime(2022, 6, 11, 0, 0, 0, 0, timezone.utc)
-
     print('Loading stock prices from "{}" to "{}"'.format(dt_from, dt_to))
     print('Loading stock prices from "{}" to "{}"'.format(dt_from.timestamp(), dt_to.timestamp()))
 
-    finnhub_api_key = os.getenv("FINNHUB_API_KEY")
-
-    # print(finnhub_api_key)
     price_count = 0
     for stock in Stock.objects.all():
         print(stock.ticker)
-        price_dict = get_daily_closing_stock_prices(finnhub_api_key, stock.ticker, dt_from, dt_to)
+        price_dict = get_closing_stock_prices_date_range(stock.ticker, dt_from, dt_to)
         # print(price_dict)
 
         for dt, prc in price_dict.items():
