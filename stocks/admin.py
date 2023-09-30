83c5edaea5d093bb37641a066d80fa04b35aaefe
steve-jimenez-ucf <144864981+steve-jimenez-ucf@users.noreply.github.com>
2023-09-30 10:52:18 -0400

    Add stock app models to admin interface

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from stocks.models import (Stock, Portfolio, StockPrice, SentimentScore, Sentiment)
+
 # Register your models here.
+admin.site.register(Stock)
+admin.site.register(Portfolio)
+admin.site.register(StockPrice)
+admin.site.register(SentimentScore)
+admin.site.register(Sentiment)
