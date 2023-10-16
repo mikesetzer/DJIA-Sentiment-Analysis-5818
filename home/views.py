0eee6e875262458c7dda7071d898f7bac0a6fe39
Mike Setzer <michaelsetzerucf@gmail.com>
2023-10-15 22:32:17 -0400

    Updates to all templates and API calls reduced to 30

--- home/views.py
+++ home/views.py
@@ -9,73 +9,71 @@ import time
 # Create your views here.
 
 def home_view(request):
-    # Example dummy data, replace with actual logic
-    stocks_list = [
-        {'symbol': 'AAPL', 'name': 'Apple Inc.', 'price': 150.00, 'change': -0.50, 'percent_change': -0.33,
-         'low': 149.50, 'high': 151.00, 'prev_close': 150.50},
-        {'symbol': 'AXP', 'name': 'American Express', 'price': 160.00, 'change': 1.50, 'percent_change': 0.94,
-         'low': 159.00, 'high': 162.00, 'prev_close': 158.50},
-        {'symbol': 'BA', 'name': 'Boeing Co.', 'price': 220.00, 'change': -2.00, 'percent_change': -0.90, 'low': 219.00,
-         'high': 223.00, 'prev_close': 222.00},
-        {'symbol': 'CAT', 'name': 'Caterpillar Inc.', 'price': 210.00, 'change': 3.00, 'percent_change': 1.44,
-         'low': 207.00, 'high': 213.00, 'prev_close': 207.00},
-        {'symbol': 'CRM', 'name': 'Salesforce', 'price': 240.00, 'change': 2.50, 'percent_change': 1.05, 'low': 238.00,
-         'high': 243.00, 'prev_close': 237.50},
-        {'symbol': 'CSCO', 'name': 'Cisco Systems', 'price': 55.00, 'change': -0.50, 'percent_change': -0.90,
-         'low': 54.50, 'high': 56.00, 'prev_close': 55.50},
-        {'symbol': 'CVX', 'name': 'Chevron Corporation', 'price': 110.00, 'change': 1.00, 'percent_change': 0.91,
-         'low': 109.00, 'high': 111.00, 'prev_close': 109.00},
-        {'symbol': 'DIS', 'name': 'Walt Disney', 'price': 180.00, 'change': -1.00, 'percent_change': -0.55,
-         'low': 179.00, 'high': 181.00, 'prev_close': 181.00},
-        {'symbol': 'DOW', 'name': 'Dow Inc.', 'price': 60.00, 'change': 0.50, 'percent_change': 0.84, 'low': 59.50,
-         'high': 60.50, 'prev_close': 59.50},
-        {'symbol': 'GS', 'name': 'Goldman Sachs', 'price': 380.00, 'change': 5.00, 'percent_change': 1.33,
-         'low': 375.00, 'high': 385.00, 'prev_close': 375.00},
-        {'symbol': 'HD', 'name': 'Home Depot', 'price': 340.00, 'change': -3.00, 'percent_change': -0.88, 'low': 337.00,
-         'high': 343.00, 'prev_close': 343.00},
-        {'symbol': 'IBM', 'name': 'IBM', 'price': 140.00, 'change': 1.50, 'percent_change': 1.08, 'low': 138.50,
-         'high': 141.50, 'prev_close': 138.50},
-        {'symbol': 'INTC', 'name': 'Intel Corp.', 'price': 53.00, 'change': -0.50, 'percent_change': -0.93,
-         'low': 52.50, 'high': 53.50, 'prev_close': 53.50},
-        {'symbol': 'JNJ', 'name': 'Johnson & Johnson', 'price': 165.00, 'change': 2.00, 'percent_change': 1.22,
-         'low': 163.00, 'high': 167.00, 'prev_close': 163.00},
-        {'symbol': 'JPM', 'name': 'JPMorgan Chase', 'price': 160.00, 'change': -1.50, 'percent_change': -0.93,
-         'low': 158.50, 'high': 161.50, 'prev_close': 161.50},
-        {'symbol': 'KO', 'name': 'Coca-Cola', 'price': 56.00, 'change': 0.00, 'percent_change': 0.00, 'low': 56.00,
-         'high': 56.00, 'prev_close': 56.00},
-        {'symbol': 'MCD', 'name': 'McDonald\'s Corp.', 'price': 240.00, 'change': 3.00, 'percent_change': 1.27,
-         'low': 237.00, 'high': 243.00, 'prev_close': 237.00},
-        {'symbol': 'MMM', 'name': '3M Co.', 'price': 190.00, 'change': -2.00, 'percent_change': -1.04, 'low': 188.00,
-         'high': 192.00, 'prev_close': 192.00},
-        {'symbol': 'MRK', 'name': 'Merck & Co.', 'price': 75.00, 'change': 1.00, 'percent_change': 1.35, 'low': 74.00,
-         'high': 76.00, 'prev_close': 74.00},
-        {'symbol': 'MSFT', 'name': 'Microsoft Corp.', 'price': 300.00, 'change': 2.00, 'percent_change': 0.67,
-         'low': 298.00, 'high': 302.00, 'prev_close': 298.00},
-        {'symbol': 'NKE', 'name': 'Nike', 'price': 150.00, 'change': 1.50, 'percent_change': 1.01, 'low': 148.50,
-         'high': 151.50, 'prev_close': 148.50},
-        {'symbol': 'PG', 'name': 'Procter & Gamble', 'price': 140.00, 'change': -1.00, 'percent_change': -0.71,
-         'low': 139.00, 'high': 141.00, 'prev_close': 141.00},
-        {'symbol': 'TRV', 'name': 'Travelers Companies Inc.', 'price': 150.00, 'change': -1.50, 'percent_change': -0.99,
-         'low': 148.50, 'high': 151.50, 'prev_close': 151.50},
-        {'symbol': 'UNH', 'name': 'UnitedHealth Group Inc.', 'price': 420.00, 'change': 4.00, 'percent_change': 0.96,
-         'low': 416.00, 'high': 424.00, 'prev_close': 416.00},
-        {'symbol': 'V', 'name': 'Visa Inc.', 'price': 230.00, 'change': -2.50, 'percent_change': -1.08, 'low': 227.50,
-         'high': 232.50, 'prev_close': 232.50},
-        {'symbol': 'VZ', 'name': 'Verizon Communications', 'price': 55.00, 'change': -0.50, 'percent_change': -0.90,
-         'low': 54.50, 'high': 55.50, 'prev_close': 55.50},
-        {'symbol': 'WBA', 'name': 'Walgreens Boots Alliance', 'price': 50.00, 'change': 0.50, 'percent_change': 1.01,
-         'low': 49.50, 'high': 50.50, 'prev_close': 49.50},
-        {'symbol': 'WMT', 'name': 'Walmart', 'price': 145.00, 'change': 0.50, 'percent_change': 0.35, 'low': 144.50,
-         'high': 145.50, 'prev_close': 144.50},
-        {'symbol': 'XOM', 'name': 'Exxon Mobil Corp.', 'price': 60.00, 'change': -0.50, 'percent_change': -0.83,
-         'low': 59.50, 'high': 60.50, 'prev_close': 60.50},
-    ]
+    # Setup client
+    finnhub_api_key = os.getenv('FINNHUB_API_KEY')  # Make sure this is set in your environment variables
+    finnhub_client = Client(api_key=finnhub_api_key)
+
+    # Hardcoded list of stock symbols and corresponding company names
+    stock_symbols_and_names = {
+        'AAPL': 'Apple Inc.',
+        'AXP': 'American Express Company',
+        'BA': 'The Boeing Company',
+        'CAT': 'Caterpillar Inc.',
+        'CRM': 'Salesforce.com, Inc.',
+        'CSCO': 'Cisco Systems, Inc.',
+        'CVX': 'Chevron Corporation',
+        'DIS': 'The Walt Disney Company',
+        'DOW': 'Dow Inc.',
+        'GS': 'The Goldman Sachs Group, Inc.',
+        'HD': 'The Home Depot, Inc.',
+        'IBM': 'International Business Machines Corporation',
+        'INTC': 'Intel Corporation',
+        'JNJ': 'Johnson & Johnson',
+        'JPM': 'JPMorgan Chase & Co.',
+        'KO': 'The Coca-Cola Company',
+        'MCD': "McDonald's Corporation",
+        'MMM': '3M Company',
+        'MRK': 'Merck & Co., Inc.',
+        'MSFT': 'Microsoft Corporation',
+        'NKE': 'NIKE, Inc.',
+        'PG': 'The Procter & Gamble Company',
+        'TRV': 'The Travelers Companies, Inc.',
+        'UNH': 'UnitedHealth Group Incorporated',
+        'V': 'Visa Inc.',
+        'VZ': 'Verizon Communications Inc.',
+        'WBA': 'Walgreens Boots Alliance, Inc.',
+        'WMT': 'Walmart Inc.',
+        'XOM': 'Exxon Mobil Corporation'
+    }
+
+    stocks_list = []
+
+    for symbol, company_name in stock_symbols_and_names.items():
+        # Fetch stock quote
+        quote = finnhub_client.quote(symbol)
+
+        if quote:
+            # Calculate change and percent_change
+            change = quote['c'] - quote['pc']
+            percent_change = (change / quote['pc']) * 100 if quote['pc'] else 0
+
+            stock_info = {
+                'symbol': symbol,
+                'name': company_name,  # Using the hardcoded company name
+                'price': '${:.2f}'.format(quote['c']),
+                'change': '$ {:.2f}'.format(change),
+                'percent_change': f'{percent_change:.2f}%',
+                'low': '${:.2f}'.format(quote['l']),
+                'high': '${:.2f}'.format(quote['h']),
+                'prev_close': '${:.2f}'.format(quote['pc']),
+            }
+
+            stocks_list.append(stock_info)
 
     context = {
         'stocks_list': stocks_list,
     }
 
-    # Page from the theme
     return render(request, 'stocks_list.html', context)
 
 
@@ -100,12 +98,30 @@ def stock_detail_view(request, symbol):
     # Assuming 'recommendation' is calculated somehow based on available data
     recommendation = "Buy"  # Placeholder, you should implement your logic here
 
+    # Calculate the percent change between the current price and the previous close price.
+    # Make sure 'previous_close' is available and is a float to avoid a ZeroDivisionError.
+    if quote.get('pc') and isinstance(quote.get('o'), float):
+        percent_change = ((quote['c'] - quote['o']) / quote['o']) * 100  # This can be negative if the price decreased
+    else:
+        percent_change = None  # or some default value, or raise an exception
+
+    # Market capitalization is typically represented in millions of dollars in financial data.
+    # We'll convert it to billions and round to two decimal places. If the market cap is not available,
+    # we default to 'N/A'. You may need to adjust this based on how your data is represented.
+
+    market_cap_in_billions = None
+    if company_info.get('marketCapitalization') is not None:
+        market_cap_in_millions = company_info['marketCapitalization']
+        market_cap_in_billions = round(market_cap_in_millions / 1000, 2)  # Convert to billions
+
     # Construct the stock information dictionary
     stock = {
         'name': company_info['name'],
         'recommendation': recommendation,
-        'price': quote['c'],  # current price
-        'market_cap': company_info.get('marketCapitalization', 'N/A'),  # Use 'N/A' if not available
+        'price': quote['c'],
+        'previous_close': quote['pc'],
+        'percent_change': f'{percent_change:.2f}' if percent_change is not None else 'N/A',
+        'market_cap': f'{market_cap_in_billions}' if market_cap_in_billions is not None else 'N/A',
         'open_price': quote['o'],
         'high_price': quote['h'],
         'low_price': quote['l'],
@@ -154,7 +170,7 @@ def stock_detail_view(request, symbol):
 
     context = {
         'stock': stock,
-        'news_list': news_list[:5],  # Limiting to 5 news items
+        'news_list': news_list[:3],  # Limiting to 5 news items
         'candlestick_data': prepared_candlestick_data,  # This can be used by a JS charting library
     }
 
